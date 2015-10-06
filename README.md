84054993ef764f232e1ee758a157ca6d9f75ee1c
yene <yene@slowmouse.ch>
2015-10-06 16:53:04 +0200

    added template from exisiting app

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ Small blog in react that uses HTTP directory listing as index, no backend needed
 * ES2015
 
 
+## Folders & Files
+* /app contains the react code
+* /build contains the generated version ready for deploy
+* /app/index.jsx is our webpack starting point
+
+## external dependencies
+* React.js is included from CDN, with local fallback
+
 ## Questions & Goals
 * How to add CSS to ES2015 modules, is there a nice way with webpack.
 * Learn a few things that then can be ported to blog software hugo.
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    context: __dirname + "/app",
+    entry: "./index.jsx",
+    output: {
+        path: "./build",
+        filename: "app.js",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel'
+            }
+        ]
+    },
+    externals: {
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: "./build"
+    }
+};
