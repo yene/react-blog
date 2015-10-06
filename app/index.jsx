462973533f048b6df0c0774080c37fd85f7c99a1
yene <yene@slowmouse.ch>
2015-10-06 18:09:31 +0200

    getting files from directory listing

--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 import PostListing from './post-listing';
 
 var request = new XMLHttpRequest();
-request.open('GET', 'posts.json', true);
+request.open('GET', 'content', true);
+
+var data = [];
 
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
-    // Success!
-    var data = JSON.parse(this.response);
+    /*
+    Parse the files out form the webservers directory listing.
+    */
+
+    var dummyElement = document.createElement('dummy');
+    dummyElement.innerHTML = this.response;
+
+    var files = dummyElement.querySelectorAll('ul#files li a')
+    for (let f of files) {
+      let name = f.querySelector('.name').innerHTML
+      if (name == '..') {
+        continue;
+      }
+
+      let date = f.querySelector('.date').innerHTML
+      data.push({"title": name, "date": date});
+    }
+
     React.render(
       <PostListing data={data}/>,
       document.getElementById('listing')
